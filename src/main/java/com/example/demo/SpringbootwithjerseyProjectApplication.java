e3a987b69f1420a4ede5e53a9714def5866e410f
Pravalikabathula <121026419+Pravalikabathula@users.noreply.github.com>
2022-12-23 11:56:57 +0530

    Update SpringbootwithjerseyProjectApplication.java

--- src/main/java/com/example/demo/SpringbootwithjerseyProjectApplication.java
+++ src/main/java/com/example/demo/SpringbootwithjerseyProjectApplication.java
@@ -6,12 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootwithjerseyProjectApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder bulider) {
-		return bulider.sources(SpringbootwithjerseyProjectApplication.class);
-	}
+public class SpringbootwithjerseyProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootwithjerseyProjectApplication.class, args);
